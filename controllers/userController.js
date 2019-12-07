7e9e34d37e0b0c4c5c9ad72b17c26fb222a11559
jastring <tripp.stringfield@gmail.com>
2019-12-07 10:04:17 -0500

    profile model

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const db = require('../models');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 module.exports = {
--- models/user.js
+++ models/user.js
@@ -5,7 +5,11 @@ const userSchema = new Schema({
   userName: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  createdOn: { type: Date, default: Date.now }
+  createdOn: { type: Date, default: Date.now },
+  profile: {
+    type: Schema.Types.ObjectId,
+    ref: "Profile"
+  }
 });
 
 const User = mongoose.model("User", userSchema);
