457c05253747eee989b0a9a8c5f48aab8aaf94ac
ShawnG <shawngway@gmail.com>
2019-12-09 20:31:49 -0500

    testNav

---
+++ client/src/components/Nav/testNav.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./Nav.css";
+
+export default class testNav extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
