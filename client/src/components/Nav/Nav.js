a8bd8493f7921c550f2778c51ef9d90cf9483de9
Charles Danner <danner.ced1@gmail.com>
2019-12-14 17:14:30 -0500

    fixed navbar href issue

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,33 +1,37 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import "./Nav.css";
 import LogInOutBtn from "../LogInOutBtn/LogInOutBtn";
 import { AuthContext } from "../../context/AuthContext";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 export default function Nav() {
   const { isAuthenticated, toggleAuth } = useContext(AuthContext);
-    
-        return (
-            <div>
-                <nav className="navbar navbar-expand navbar-dark bg-primary">
-                    <a className="navbar-brand" href="/">
-                        Base
-                    </a>
-                    {/* add "active" to the nav-item className for what is active */}
-                    <ul class="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Allies<span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">nav item</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#"></a>
-                        </li>
-                    </ul>
-                    <LogInOutBtn />
 
-                </nav>
-            </div>
-        )
-    
+  return (
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-primary">
+        <Link className="navbar-brand" to="/">
+          Base
+        </Link>
+
+        {/* add "active" to the nav-item className for what is active */}
+        <ul class="navbar-nav mr-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Allies<span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              nav item
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#"></a>
+          </li>
+        </ul>
+        <LogInOutBtn />
+      </nav>
+    </div>
+  );
 }
