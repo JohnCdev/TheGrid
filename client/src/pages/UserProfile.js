389036cfc2e67aea7f17c878d8a804ebe0f53b58
Charles Danner <danner.ced1@gmail.com>
2019-12-17 12:13:21 -0500

    page conditionally renders different content based on whether or not a profile is returned from get request.

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -3,31 +3,42 @@ import { Link, withRouter } from "react-router-dom";
 import API from "../utils/API";
 import { Container } from "../components/Grid/Grid";
 
-
-
-
-
 class ViewUserProfile extends Component {
+  state = {
+    id: "",
+    currentCity: "",
+    firstName: "",
+    lastName: "",
+    userName: "",
+    age: ""
+  };
 
-    componentDidMount(){
-        const profile = this.props.match.params.userProfile
-        console.log(profile)
-        API.getUserProfile(profile)
-        .then(res => {
-            console.log(res)
-            //update state and get the components to update
-        })
-    }
-    
-    
+  componentDidMount() {
+    const profile = this.props.match.params.userProfile;
+    console.log(profile);
+    API.getUserProfile(profile).then(res => {
+      console.log(res.data.data[0]);
+      const userProfile = res.data.data[0];
+      userProfile === undefined
+        ? this.setState({ id: null })
+        : this.setState({
+            id: userProfile._id,
+            currentCity: userProfile.currentCity,
+            firstName: userProfile.firstName,
+            lastName: userProfile.lastName,
+            userName: userProfile.userName,
+            age: userProfile.age
+          });
+
+      //update state and get the components to update
+    });
+  }
 
   render() {
-
-    const profile = this.props.match.params.userProfile
-    return (
-      <Container>
-         {profile}
-      </Container>
+    return this.state.id === null ? (
+      <Container>No Profile Found</Container>
+    ) : (
+      <Container>{this.state.userName}</Container>
     );
   }
 }
