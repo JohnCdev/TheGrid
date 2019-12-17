9f4846cd233b0f94f264b314fe13dc2cb242d5ea
John Carver <29667140+JCppe@users.noreply.github.com>
2019-12-16 19:35:00 -0500

    removed comment from debug, removed uneeded props

--- client/src/components/LogInOutBtn/LogInOutBtn.js
+++ client/src/components/LogInOutBtn/LogInOutBtn.js
@@ -2,8 +2,8 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 
-export default function LogInOutBtn(props) {
-  const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+export default function LogInOutBtn() {
+  const { isAuthenticated } = useContext(AuthContext);
   const authenticationStatus = !isAuthenticated ? (
     <button className="btn my-2 my-sm-0">
       <Link to="/log-in">Log In</Link>
