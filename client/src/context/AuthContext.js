15e52b73d7feefc888fb5b0a7495d7997c435be3
Charles Danner <danner.ced1@gmail.com>
2019-12-17 16:20:27 -0500

    made the login function only run if the session storage wasn't null or undefined.

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -4,40 +4,45 @@ export const AuthContext = createContext();
 
 class AuthContextProvider extends Component {
   state = {
-    logInFunction: (userObject) => {
-      console.log(userObject.user.user)
-        this.setState({
-            isAuthenticated: true,
-            userData: {
-              userName: userObject.user.user,
-              token: userObject.user.token
-            }
-        })
+    logInFunction: userObject => {
+      console.log(userObject.user.user);
+      this.setState({
+        isAuthenticated: true,
+        userData: {
+          userName: userObject.user.user,
+          token: userObject.user.token
+        }
+      });
     },
     isAuthenticated: false
   };
 
   toggleAuth = () => {
-    console.log("auth toggle")
+    console.log("auth toggle");
     this.setState({ isAuthenticated: !this.state.isAuthenticated });
   };
 
   render() {
-    if(!this.state.isAuthenticated){
-      const user = JSON.parse(sessionStorage.getItem('project3user'))
-      const userObject = {
-        user: {
-          user: user.user,
-          token: user.token
-        }
+    if (!this.state.isAuthenticated) {
+      const user = JSON.parse(sessionStorage.getItem("project3user"));
+ 
+      if(user !== undefined && user !== null){
+        const userObject = {
+          user: {
+            user: user.user,
+            token: user.token
+          }
+        };
+        this.state.logInFunction(userObject)
       }
-      this.state.logInFunction(userObject)
     }
     return (
-        <AuthContext.Provider value={{...this.state, toggleAuth: this.toggleAuth}}>
-            { this.props.children }
-        </AuthContext.Provider>
-    )
+      <AuthContext.Provider
+        value={{ ...this.state, toggleAuth: this.toggleAuth }}
+      >
+        {this.props.children}
+      </AuthContext.Provider>
+    );
   }
 }
 
