fc6dab9df6ca914040329437e0d1219231b5c279
Charles Danner <danner.ced1@gmail.com>
2019-12-18 19:15:38 -0500

    pulled a bunch of stuff now ready for PR.

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,7 +9,7 @@ router.route('/create-new-user')
   .post(userController.create);
 
 router.route('/get-profile/:profile')
-  .get(userController.getUserProfile)  
+  .get(userController.getUserProfile)
   
 router.route('/profile')
   .post(userController.getProfile)
