674cf08bd99d895462630ba15a5a5df31785ab8f
John Carver <29667140+JCppe@users.noreply.github.com>
2020-01-01 21:02:27 -0500

    initial page

---
+++ client/src/pages/YourClansPage.js
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from 'react';
+import { Container } from '../components/Grid/Grid';
+import Header from '../components/Header/Header';
+
+const YourClansPage = () => {
+
+    useEffect(() => {
+
+    }
+    ,[])
+
+    return (
+        <main>
+            <Container>
+                <Header headerText="Your Clans"/>
+            </Container>
+        </main>
+    )
+}
+
+export default  YourClansPage;  
