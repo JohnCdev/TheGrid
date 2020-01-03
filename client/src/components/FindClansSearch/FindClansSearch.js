e46e169d3e2c69fda318ed2038e9cee572c5fe53
John Carver <29667140+JCppe@users.noreply.github.com>
2020-01-02 20:04:28 -0500

    update clan search to use refNames, need profile images to be return from search

--- client/src/components/FindClansSearch/FindClansSearch.js
+++ client/src/components/FindClansSearch/FindClansSearch.js
@@ -9,15 +9,15 @@ import API from '../../utils/API';
 const FindClansSearch = () => {
     const [clanSearch, setSearch] = useState('');
     const [clanResult, setClanResult] = useState([
-        { _id: 1, clanName: "Expo", profileImg: "Clan1" },
-        { _id: 2, clanName: "Shawn", profileImg: "Clan2" },
-        { _id: 3, clanName: "Charles", profileImg: "Clan3" },
-        { _id: 4, clanName: "John", profileImg: "Clan1" },
-        { _id: 5, clanName: "Shawn", profileImg: "Clan2" },
-        { _id: 6, clanName: "Charles", profileImg: "Clan3" },
-        { _id: 7, clanName: "John", profileImg: "Clan1" },
-        { _id: 8, clanName: "Shawn", profileImg: "Clan2" },
-        { _id: 9, clanName: "Charles", profileImg: "Clan3" }
+        // { _id: 1, clanName: "Expo", profileImg: "Clan1" },
+        // { _id: 2, clanName: "Shawn", profileImg: "Clan2" },
+        // { _id: 3, clanName: "Charles", profileImg: "Clan3" },
+        // { _id: 4, clanName: "John", profileImg: "Clan1" },
+        // { _id: 5, clanName: "Shawn", profileImg: "Clan2" },
+        // { _id: 6, clanName: "Charles", profileImg: "Clan3" },
+        // { _id: 7, clanName: "John", profileImg: "Clan1" },
+        // { _id: 8, clanName: "Shawn", profileImg: "Clan2" },
+        // { _id: 9, clanName: "Charles", profileImg: "Clan3" }
     ]);
     const [isLoading, setIsLoading] = useState(false);
 
@@ -37,6 +37,7 @@ const FindClansSearch = () => {
     }
 
     const searchResult = () => {
+        console.log(clanResult)
         return (
             <>
                 {clanResult.length > 0 ?
@@ -50,7 +51,7 @@ const FindClansSearch = () => {
                                     <span>{result.clanName}</span>
                                 </div>
                                 <div style={{ 'textAlign': 'left' }}>
-                                    <Link to={`/clans/${result.clanName}`}>
+                                    <Link to={`/clans/${result.clanReferenceName}`}>
                                         <button className="btn btn-primary">Go to Clan's Page</button>
                                     </Link>
                                 </div>
