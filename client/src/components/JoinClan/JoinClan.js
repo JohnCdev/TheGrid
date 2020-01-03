807d7a89a39efcd55817763a291409a94136e194
Charles Danner <danner.ced1@gmail.com>
2020-01-03 17:19:50 -0500

    page doesn't blow up on reload. added a conditional in component to return nothing if context isn't active.

--- client/src/components/JoinClan/JoinClan.js
+++ client/src/components/JoinClan/JoinClan.js
@@ -3,24 +3,28 @@ import {AuthContext} from '../../context/AuthContext';
 
 export default function JoinClan(props) {
   const {userData} = useContext(AuthContext)
-  const member = userData.clans.includes(props.clanName)
-  const button = !member ? (
-    <button
-      type="button"
-      className="btn btn-primary"
-      onClick={() => props.joinClan()}
-    >
-      Join Clan
-    </button>
-  ) : (
-    <button
-      type="button"
-      className="btn btn-primary"
-      onClick={() => props.leaveClan()}
-    >
-      Leave Clan
-    </button>
-  );
+  let button;
+  if(userData.clans){
+    const member = userData.clans.includes(props.clanName)
+    button = !member ? (
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => props.joinClan()}
+      >
+        Join Clan
+      </button>
+    ) : (
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => props.leaveClan()}
+      >
+        Leave Clan
+      </button>
+    );
+  } else button = ""
+
 
   return button;
 }
